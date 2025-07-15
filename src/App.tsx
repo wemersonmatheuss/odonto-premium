4d0449c2d0da0e2d5483a5db3e0275e8c737f55c
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-15 20:00:11 -0300

    feat: cria area principal(main)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import {} from "./global.css"
 import styles from "./app.module.css"
 
 import { Nav } from "./components/Nav"
+import { Main } from "./components/Main-initial"
 
 export function App() {
   return (
     <div className={styles.container}>
+      <Main/>
       <Nav/>
     </div>
   )
---
+++ src/components/Main-initial/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css"
+
+import { ButtonPrimary } from "../ButtonPrimary"
+
+export function Main() {
+    return (
+        <main className={styles.container}>
+
+            <div className={styles.title}>
+                <h1>Cuidando da Sua Saúde com Excelência</h1>
+                <p>Tecnologia de última geração e uma equipe experiente para oferecer tratamentos qualificados com segurança e qualidade.</p>
+            </div>
+
+            <div className={styles.buttons}>
+                <ButtonPrimary name="Agendar Consulta" />
+                <ButtonPrimary name="Nossas Especialidades" />
+            </div>
+        </main>
+    )
+}
