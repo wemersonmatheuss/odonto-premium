f73b02acf30311536da946a6264e6f6f8388fb23
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-15 21:04:30 -0300

    feat: criar seção de necessidades

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import styles from "./app.module.css"
 
 import { Nav } from "./components/Nav"
 import { Main } from "./components/Main-initial"
+import { Need } from "./components/Need"
 
 export function App() {
   return (
     <div className={styles.container}>
       <Main/>
       <Nav/>
+      <Need />
     </div>
   )
 }
---
+++ src/components/Need/index.tsx
@@ -0,0 +1,56 @@
+import styles from "./styles.module.css"
+
+import img from "../../assets/image-need.jpg"
+
+export function Need() {
+    return (
+        <section className={styles.container}>
+            <h1>Por que você precisa da gente</h1>
+
+            <div className={styles.containerChild}>
+                <div className={styles.needs}>
+
+                    <div className={styles.need}>
+
+                        <div className={styles.img}></div>
+
+                        <div className={styles.description}>
+                            <h3>Necessidade 1</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis dolores ipsum vel incidunt, voluptas earum amet laboriosam sint. Voluptates in veniam minima repudiandae mollitia facilis consequuntur quisquam! Odio, perspiciatis reprehenderit.</p>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.need}>
+
+                        <div className={styles.img}></div>
+
+                        <div className={styles.description}>
+                            <h3>Necessidade 2</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis dolores ipsum vel incidunt, voluptas earum amet laboriosam sint. Voluptates in veniam minima repudiandae mollitia facilis consequuntur quisquam! Odio, perspiciatis reprehenderit.</p>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.need}>
+
+                        <div className={styles.img}></div>
+
+                        <div className={styles.description}>
+                            <h3>Necessidade 3</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis dolores ipsum vel incidunt, voluptas earum amet laboriosam sint. Voluptates in veniam minima repudiandae mollitia facilis consequuntur quisquam! Odio, perspiciatis reprehenderit.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className={styles.image}>
+                    <img src={img} alt="Imagem da logo da clinica odonto premium" />
+                </div>
+            </div>
+
+            
+        </section>
+    )
+}
