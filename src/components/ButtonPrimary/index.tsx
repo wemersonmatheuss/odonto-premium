a9d6545ccf3768143ae30ec3c0deaad54485fe20
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-19 12:03:21 -0300

    feat: inicar responsividade para 320

--- src/components/ButtonPrimary/index.tsx
+++ src/components/ButtonPrimary/index.tsx
@@ -1,11 +1,9 @@
-import styles from "./styles.module.css"
-
 type Props = React.ComponentProps<"button"> & {
     name: string
 }
 
 export function ButtonPrimary({ name, ...rest }: Props) {
     return (
-        <button className={styles.button} { ...rest } type="button">{name}</button>
+        <button { ...rest } type="button">{name}</button>
     )
 }
